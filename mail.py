d5aaf5723e64b6cd2736d81932727d8b3fa87c43
Stella0910 <aniretake0910@gmail.com>
2025-03-28 14:33:35 +0300

    Add files via upload

--- mail.py
+++ mail.py
@@ -10,22 +10,20 @@ def has_upper_letters(password):
     return any(character.isupper() for character in password)
 
 
-def  has_lower_letters(password):
+def has_lower_letters(password):
     return any(character.islower() for character in password)
 
 
 def has_symbols(password):
-    return any(not character.isdigit() and not character.isalpha() for character in password)
-
-
-password_checks = [is_very_long, has_digit, has_upper_letters, has_lower_letters, has_symbols]
+    return any(not character.isdigit() and not character.isalpha()
+               for character in password)
 
 
 def main():
+    password_checks = [is_very_long, has_digit, has_upper_letters,
+                       has_lower_letters, has_symbols]
     score = 0
-
     password_option = input("Введите пароль: ")
-
     for check in password_checks:
         if check(password_option):
             score = score + 2
